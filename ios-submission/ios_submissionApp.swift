54887d42ab47d7ec53cca53b6d8f5fa0498a9279
magistraapta <magsitraagis@gmail.com>
2024-01-24 14:09:24 +0800

    Initial Commit

---
+++ ios-submission/ios_submissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_submissionApp.swift
+//  ios-submission
+//
+//  Created by Magistra Apta on 24/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_submissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
